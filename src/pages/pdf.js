015a06c2ef8788a500a186d30de2f5859310b3d5
BoraALAP <bora.alap@artticfox.com>
2020-10-17 18:22:52 -0400

    push

---
+++ src/pages/pdf.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+import PDF from '../assets/doug_letter.pdf'
+ 
+const pdf = (props) => {
+  return (
+    <PDF />
+  );
+};
+ 
+const Container = styled.div`
+`;
+ 
+export default pdf
